fc7cbd386ea5087dc86ade54373566ce43958090
Zaiyan Azeem <zaiyanazeem@gmail.com>
2025-05-27 16:44:33 -0400

    Navbar Complete

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+// mobile menu toggle
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
-const navList = document.querySelector('.nav-list');
+const navList         = document.querySelector('.nav-list');
 
 mobileNavToggle.addEventListener('click', () => {
   navList.classList.toggle('active');
   mobileNavToggle.classList.toggle('open');
+});
+
+// change navbar on scroll
+const header = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('solid');
+    header.classList.remove('transparent');
+  } else {
+    header.classList.add('transparent');
+    header.classList.remove('solid');
+  }
 });
